b1338462ffb8ba7681d3e62af3a07139d8c36236
zyc <2375850344@qq.com>
2023-09-05 15:12:13 +0800

    zyc_sec_commit

--- single_cycle/cpu_test.asm
+++ single_cycle/cpu_test.asm
@@ -4,20 +4,33 @@
 .text
 	ori t0, zero, 0x300
 	ori t1, zero, 0x45
+	nop
+	nop
+	nop
 	add t2, t0, t1
 	sub t3, t0, t1
 	lui t4, 0x12
+	nop
+	nop
+	nop
 	add t4, t4, t2
 	jal label1
 	ori t6, zero, 0x1
 label1:
 	lui a0, 0x40
-	sw t4, (a0)
+	nop
+	nop
 	ori a1, zero, 0x4
+	sw t4, (a0)
+	nop
+	nop
 	add a1, a0, a1
+	nop
+	nop
+	#lw t5, (a0)
 	sw t4, (a1) 
-	lw t5, (a0)
-	beq t4, t5, label2
+label3:
+	beq t4, t5, label2 #差分支预测错误检�?
 	ori t6, zero, 0x2
 label2:
-	jal label2
+	jal label3
--- single_cycle/single_cycle.srcs/sources_1/new/myCPU/riscv_top.v
+++ single_cycle/single_cycle.srcs/sources_1/new/myCPU/riscv_top.v
@@ -33,7 +33,7 @@ module mycpu(
     input  [31:0] data_ram_rdata
 );
     
-    wire [31:0] pc_in;
+    wire flag_D;
     wire [31:0] pc_out;
     wire [31:0] pc_pred;
     wire [31:0] select_pc_out;
@@ -73,12 +73,51 @@ module mycpu(
     wire [11:0] imm12_b;
     wire [19:0] imm20_j;
     wire [31:0] offset;
+        
+    wire [31:0] pc_D;
+    wire [6:0] opcode_D;
+    wire [2:0] funct3_D;
+    wire [6:0] funct7_D;
+    wire [4:0] reg_ra1_D;
+    wire [4:0] reg_ra2_D;
+    wire [4:0] reg_wa_D;
+    wire [11:0] imm12_s_D;
+    wire [11:0] imm12_i_D;
+    wire [31:0] imm20_e32_D;
     
+    wire [31:0] pc_E;
+    wire [31:0] alu_in1_E;
+    wire [31:0] alu_in2_E;
+    wire flag_E;
+    wire [31:0] reg_wa_E;
+    wire [4:0] alu_op_E; 
+    wire [31:0] imm20_e32_E;
+    wire [1:0] c3_E;
+    wire dmem_we_E;
+    wire reg_we_E;
+    wire [31:0] reg_rd2_E;
+    
+    wire [31:0] pc_M;     
+    wire [31:0] alu_out_M;     
+    wire [4:0]  reg_wa_M;     
+    wire [31:0] imm20_e32_M;     
+    wire [1:0]  c3_M;     
+    wire        dmem_we_M;     
+    wire        reg_we_M;     
+    wire [31:0] reg_rd2_M; 
+    
+    wire [31:0] pc_W;     
+    wire [31:0] alu_out_W;     
+    wire [4:0]  reg_wa_W;     
+    wire [31:0] imm20_e32_W;     
+    wire [1:0]  c3_W;     
+    wire        reg_we_W;     
+    wire [31:0] dmem_rd_W;      
     //assign pc_in = pc_out + (jump ? offset : 32'h4);
     
-    assign select_pc_out = flag ? pc_out : pc_E +32'h4;
+    assign select_pc_out = flag_E ? pc_out : pc_E +32'h4;
     
-    assign inst_rom_addr = pc_out;
+    assign inst_rom_addr = select_pc_out;
     assign instr = inst_rom_rdata;
 
     assign funct7 = instr[31:25];
@@ -91,8 +130,8 @@ module mycpu(
     
     assign imm20_u = instr[31:12];
     assign imm20_e32 = {imm20_u, 12'b0};
-    assign pc_plus4 = pc_out + 32'h4;
-    assign reg_wd = sel_2to4(c3, imm20_e32, pc_plus4, alu_out, dmem_rd);
+    assign pc_plus4 = pc_W + 32'h4;
+    assign reg_wd = sel_2to4(c3_W, imm20_e32_W, pc_plus4, alu_out_W, dmem_rd_W);
     function [31:0] sel_2to4(input [1:0] sel,
                              input [31:0] in0, input [31:0] in1,
                              input [31:0] in2, input [31:0] in3);
@@ -109,12 +148,12 @@ module mycpu(
     assign alu_in1 = reg_rd1;
     assign imm12_s = {instr[31:25], instr[11:7]};
     assign imm12_i = instr[31:20];
-    assign imm12_e32 = c1 ? {{20{imm12_s[11]}}, imm12_s} : {{20{imm12_i[11]}}, imm12_i};
+    assign imm12_e32 = c1 ? {{20{imm12_s_D[11]}}, imm12_s_D} : {{20{imm12_i_D[11]}}, imm12_i_D};
     assign alu_in2 = c2 ? reg_rd2 : imm12_e32;
     
-    assign data_ram_addr = alu_out;
-    assign data_ram_wen = dmem_we;
-    assign data_ram_wdata = reg_rd2;
+    assign data_ram_addr = alu_out_M;
+    assign data_ram_wen = dmem_we_M;
+    assign data_ram_wdata = reg_rd2_M;
     assign dmem_rd = data_ram_rdata;
 
     assign imm12_b = {instr[31], instr[7], instr[30:25], instr[11:8]};
@@ -123,14 +162,41 @@ module mycpu(
     pc _pc(
         .clk(clk),
         .rst(rstn),
-        .pc_in(pc_in),
-        .pc_out(pc_out)
+        .pc_pred(pc_pred),
+        .pc_sel_in(pc_out)
+    );
+
+    
+    pipeline_reg_D _pipeline_reg_D(
+        .rst(rstn),
+        .clk(clk),
+        .nop(flag_E),
+        .pc_D(select_pc_out),       
+        .opcode(opcode),     
+        .funct3(funct3),     
+        .funct7(funct7),     
+        .reg_ra1(reg_ra1),    
+        .reg_ra2(reg_ra2),   
+        .reg_wa(reg_wa),
+        .imm12_s(imm12_s),
+        .imm12_i(imm12_i),
+        .imm20_e32(imm20_e32),
+        ._pc_D(pc_D),
+        ._opcode(opcode_D),
+        ._funct3(funct3_D),
+        ._funct7(funct7_D),
+        ._reg_ra1(reg_ra1_D),
+        ._reg_ra2(reg_ra2_D),
+        ._reg_wa(reg_wa_D),
+        ._imm12_s(imm12_s_D),
+        ._imm12_i(imm12_i_D),
+        ._imm20_e32(imm20_e32_D)
     );
 
     control _cu(
-        .opcode(opcode),
-        .funct3(funct3),
-        .funct7(funct7),
+        .opcode(opcode_D),
+        .funct3(funct3_D),
+        .funct7(funct7_D),
         .c1(c1),
         .c2(c2),
         .c3(c3),
@@ -143,31 +209,100 @@ module mycpu(
     regfile _regfile(
         .clk(clk),
         .rst(rstn),
-        .reg_we(reg_we),
-        .reg_ra1(reg_ra1),
-        .reg_ra2(reg_ra2),
-        .reg_wa(reg_wa),
+        .reg_we(reg_we_W),
+        .reg_ra1(reg_ra1_D),
+        .reg_ra2(reg_ra2_D),
+        .reg_wa(reg_wa_W),
         .reg_wd(reg_wd),
         .reg_rd1(reg_rd1),
         .reg_rd2(reg_rd2)
     );
-
-    alu _alu(
-        .alu_op(alu_op),
+    
+         
+    pipeline_reg_E _pipeline_reg_E(
+        .rst(rstn),
+        .clk(clk),
+        .pc_E(pc_D),
         .alu_in1(alu_in1),
         .alu_in2(alu_in2),
+        .flag(flag_D),
+        .reg_wa(reg_wa_D),
+        .alu_op(alu_op),
+        .imm20_e32(imm20_e32_D),
+        .c3(c3),
+        .dmem_we(dmem_we),
+        .reg_we(reg_we),
+        .reg_rd2(reg_rd2),    
+        ._pc_E(pc_E),
+        ._alu_in1(alu_in1_E),
+        ._alu_in2(alu_in2_E),
+        ._flag(flag_E),   
+        ._reg_wa(reg_wa_E),
+        ._alu_op(alu_op_E),
+        ._imm20_e32(imm20_e32_E),
+        ._c3(c3_E),
+        ._dmem_we(dmem_we_E),
+        ._reg_we(reg_we_E),   
+        ._reg_rd2(reg_rd2_E)                 
+    );
+
+    alu _alu(
+        .alu_op(alu_op_E),
+        .alu_in1(alu_in1_E),
+        .alu_in2(alu_in2_E),
         .alu_out(alu_out)
+    );    
+    
+    pipeline_reg_M _pipeline_reg_M(
+        .rst(rstn),
+        .clk(clk),
+        .pc_M(pc_E),
+        .alu_out(alu_out),
+        .reg_wa(reg_wa_E),
+        .imm20_e32(imm20_e32_E),
+        .c3(c3_E),
+        .dmem_we(dmem_we_E),
+        .reg_we(reg_we_E),
+        .reg_rd2(reg_rd2_E),       
+        ._pc_M(pc_M),
+        ._alu_out(alu_out_M),
+        ._reg_wa(reg_wa_M),
+        ._imm20_e32(imm20_e32_M),
+        ._c3(c3_M),
+        ._dmem_we(dmem_we_M),
+        ._reg_we(reg_we_M),
+        ._reg_rd2(reg_rd2_M)           
+    );
+       
+    
+    pipeline_reg_W _pipeline_reg_W(
+        .rst(rstn),
+        .clk(clk),
+        .pc_W(pc_M),
+        .alu_out(alu_out_M),
+        .reg_wa(reg_wa_M),
+        .imm20_e32(imm20_e32_M),
+        .c3(c3_M),
+        .reg_we(reg_we_M),
+        .dmem_rd(dmem_rd),       
+        ._pc_W(pc_W),
+        ._alu_out(alu_out_W),
+        ._reg_wa(reg_wa_W),
+        ._imm20_e32(imm20_e32_W),
+        ._c3(c3_W),
+        ._reg_we(reg_we_W),
+        ._dmem_rd(dmem_rd_W)           
     );
     
-    br _br(
-        .branch(branch),
-        .imm12_b(imm12_b),
-        .imm20_j(imm20_j),
-        .reg_rd1(reg_rd1),
-        .reg_rd2(reg_rd2),
-        .jump(jump),
-        .offset(offset)
-    );
+//    br _br(
+//        .branch(branch),
+//        .imm12_b(imm12_b),
+//        .imm20_j(imm20_j),
+//        .reg_rd1(reg_rd1),
+//        .reg_rd2(reg_rd2),
+//        .jump(jump),
+//        .offset(offset)
+//    );
     
     pred_pc _pred_pc(
         .imm12_b(imm12_b),
@@ -181,7 +316,7 @@ module mycpu(
         .branch(branch),
         .reg_rd1(reg_rd1),
         .reg_rd2(reg_rd2),        
-        .flag(flag)
+        .flag(flag_D)
     );
 
 endmodule
